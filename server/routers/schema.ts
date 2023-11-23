bd8b5ab81c178aeed0b6c2ecc792ff03fbf63b26
Chris Anderson <chris.d.anderson87@gmail.com>
2023-11-23 18:25:38 +1300

    added in other CRUD routes and tested with postman

--- server/routers/schema.ts
+++ server/routers/schema.ts
@@ -1,16 +1,15 @@
-import { Schema } from 'mongoose';
+import { Schema } from "mongoose";
 
 export interface Pool {
-    date: Date;
-    category: string;
-    price: number;
+  date: Date;
+  category: string;
+  price: number;
 }
 
 const pool = new Schema<Pool>({
-    date: { type: 'date', required: true},
-    category: { type: 'string', required: true},
-    price: { type: 'number', required: true}
-})
+  date: { type: "date" },
+  category: { type: "string", required: true },
+  price: { type: "number", required: true },
+});
 
-export default pool 
-
+export default pool;
