7d4079849778742771a6da11374802c5f91032e9
Chris Anderson <chris.d.anderson87@gmail.com>
2023-11-23 17:31:48 +1300

    removing disconnect for now. will add ticket to investigate connection tests

--- server/routers/model.ts
+++ server/routers/model.ts
@@ -6,9 +6,5 @@ const run = async () => {
   await connect("mongodb://127.0.0.1:27017/budget");
 };
 
-const closeConnection = async () => {
-  await disconnect(); // Assuming disconnect is how you close the connection
-};
-
 export const newModel = model<Pool>("newModel", pool, "newModel");
 run().catch((err) => console.log(err));
