e40a4a1bb5960c103a68bc164219f13cbd19f14d
Chris Anderson <chris.d.anderson87@gmail.com>
2023-11-23 15:55:10 +1300

    resolved bug from jest mongodb connection remaining open

--- server/routers/model.ts
+++ server/routers/model.ts
@@ -1,10 +1,19 @@
-import { model, connect } from 'mongoose';
-import pool from './schema';
-import { Pool } from './schema';
+import { model, connect, disconnect } from "mongoose";
+import pool from "./schema";
+import { Pool } from "./schema";
 
 const run = async () => {
-    await connect('mongodb://127.0.0.1:27017/budget');
-}
+  await connect("mongodb://127.0.0.1:27017/budget");
+};
 
-export const newModel = model<Pool>('newModel', pool, 'newModel');
-run().catch(err => console.log(err));
+const closeConnection = async () => {
+  await disconnect(); // Assuming disconnect is how you close the connection
+};
+
+// Close the connection after all tests are finished
+afterAll(async () => {
+  await closeConnection();
+});
+
+export const newModel = model<Pool>("newModel", pool, "newModel");
+run().catch((err) => console.log(err));
