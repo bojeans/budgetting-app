fc430475082ce99cde075175cceb4b0a58fa7c60
Chris Anderson <chris.d.anderson87@gmail.com>
2023-11-23 14:54:59 +1300

    adding in utility function for id converter

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from 'express';
+import { Document, Types } from 'mongoose';
 import { newModel } from './routers/model';
+import formatID from './utils/formatID';
 
 const app = express();
 const json = express.json();
 
-interface RequestHandler {
-    (req: Request, res: Response): any;
+// interfaces
+interface Pool {
+  _id: Types.ObjectId; 
 }
 
 // middleware
@@ -13,13 +16,14 @@ app.use(json);
 
 // routes
 app.get('/budget', async (req: Request, res: Response) => {
-    try{
-        const data = await newModel.find();
-        res.json(data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error!!'})
-    }
+  try {
+    const data: Document<Pool>[] = await newModel.find(); 
+    const transformedData = data.map((doc) => formatID({ ...doc.toObject(), _id: doc._id.toString() }));
+    res.json(transformedData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error!!' });
+  }
 });
 
 export default app;
--- server/routers/model.ts
+++ server/routers/model.ts
@@ -6,5 +6,5 @@ const run = async () => {
     await connect('mongodb://127.0.0.1:27017/budget');
 }
 
-export const newModel = model<Pool>('newModel', pool);
+export const newModel = model<Pool>('newModel', pool, 'newModel');
 run().catch(err => console.log(err));
